087af64f2bb46b3c4156752a58b54eb3abae7abd
Sohaib <sohaib-ijaz14@hotmail.com>
2023-07-24 00:13:46 +0200

    init 3

---
+++ src/main/java/com/example/visaapp/EmailUtil.java
@@ -0,0 +1,66 @@
+package com.example.visaapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class EmailUtil {
+
+
+
+
+    /**
+     * Utility method to send simple HTML email
+     * @param session
+     * @param toEmail
+     * @param subject
+     * @param body
+     */
+    public static void sendEmail(Session session, String toEmail, String subject, String body){
+
+        try
+        {
+            MimeMessage msg = new MimeMessage(session);
+            //set message headers
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress("no-reply@sohaib.com", "Visa Appointment"));
+
+            msg.setReplyTo(InternetAddress.parse("no-reply@sohaib.com", false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setText(body, "UTF-8");
+
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+            System.out.println("Message is ready");
+            Transport.send(msg);
+
+            System.out.println("EMail Sent Successfully!!");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/visaapp/VisaAppApplication.java
+++ src/main/java/com/example/visaapp/VisaAppApplication.java
@@ -3,13 +3,80 @@ package com.example.visaapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
 @SpringBootApplication
 public class VisaAppApplication {
 
-    public static void main(String[] args) {
+    private static String APP_PROP_FILE_NAME = "application.properties";
+
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(VisaAppApplication.class, args);
-        System.out.println("Hello World! 123465");
-        System.out.println("Hello World!");
+
+        boolean found = true;
+
+        URL url = new URL("https://appointment.bmeia.gv.at/HomeWeb/Scheduler");
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.setRequestProperty("Content-Type", "application/json");
+
+        String data = "{\"office\": \"islamabad\",\n\"calendarid\":\"21836737\",\n\"personcount\":\"1\"}";
+
+        byte[] out = data.getBytes(StandardCharsets.UTF_8);
+
+        OutputStream stream = http.getOutputStream();
+        stream.write(out);
+
+        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+        BufferedReader br = new BufferedReader(new InputStreamReader(http.getInputStream()));
+        String strCurrentLine;
+        while ((strCurrentLine = br.readLine()) != null) {
+           // System.out.println(strCurrentLine);
+            if(strCurrentLine.contains("For your selection there are unfortunately no appointments available")){
+                found = false;
+            }
+        }
+
+        if(found){
+            System.out.println("Appointment found");
+            sendEmail();
+        }else{
+            System.out.println("No Appointments found!");
+        }
+        http.disconnect();
+        //sendEmail();
+    }
+
+    private static void sendEmail() throws IOException {
+        InputStream inputStream = VisaAppApplication.class.getClassLoader().getResourceAsStream(APP_PROP_FILE_NAME);
+        Properties props = new Properties();
+        props.load(inputStream);
+
+        final String fromEmail = props.getProperty("from.email");
+        final String password = props.getProperty("fromPs");
+        final String toEmail = props.getProperty("from.email");
+
+        System.out.println("TLSEmail Start");
+
+        //create Authenticator object to pass in Session.getInstance argument
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, password);
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+
+        EmailUtil.sendEmail(session, toEmail, "ISB Embassy Appointment", "Appointments found for the following dates!");
     }
 
 }
