1b32c9549bbc9e0ad44558ecb30b50309a9d1d82
Sohaib <sohaib-ijaz14@hotmail.com>
2023-07-23 17:02:59 +0200

    Initial commit

---
+++ src/main/java/com/example/visaapp/VisaAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.visaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisaAppApplication.class, args);
+        System.out.println("Hello World! 123465");
+    }
+
+}
